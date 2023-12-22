5dfab5b1e088a2c2c2aa2c162257c20366793756
gonzuiswimmer <gonzuiswimmer@gmail.com>
2023-12-22 09:19:56 +0900

    Dash_25 ログイン時に管理者かどうかを判別
    * loginメソッドに管理者かどうかを判別する処理を記述し、フロントへ情報をリターン

--- src/php/app/Http/Controllers/Auth/LoginController.php
+++ src/php/app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
@@ -27,7 +28,15 @@ class LoginController extends Controller
         ]);
 
         if (auth()->attempt($credentials)) {
-            return response()->json([Auth::User()], 200);
+            $role = '';
+            $user = User::with(['role'])->find(Auth::id());
+            // 管理者ユーザーか一般ユーザーか判別してフロントに渡す
+            if(isset($user->role) && $user->role->role == 0) {
+                $role = 'admin';
+            } else {
+                $role = 'other';
+            };
+            return response()->json(['role'=>$role], 200);
         }
 
         return response()->json(['message' => 'ユーザーが見つかりません。'], 422);
