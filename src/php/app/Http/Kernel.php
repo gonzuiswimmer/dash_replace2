e261545c079c4b9f15f40a2de4895d010418d7e3
gonzuiswimmer <gonzuiswimmer@gmail.com>
2023-12-22 09:18:59 +0900

    Dash_25 api用のミドルウェアを用意
    * api用にミドルウェアを用意し、ルートなどを修正

--- src/php/app/Http/Kernel.php
+++ src/php/app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'judgeAdmin' => \App\Http\Middleware\JudgeAdminMiddleware::class,
+        'judgeApiAdmin' => \App\Http\Middleware\JudgeAdminApiMiddleware::class,
     ];
 }
